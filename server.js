f8eb61060495176c4f88a94bad39085c371ef9db
Minh To <minhto@Minhs-MacBook-Pro.local>
2019-09-06 15:31:41 -0400

    testing

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello jhhjhh!");
 });
 
 // setup http server to listen on HTTP_PORT
