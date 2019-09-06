de6b13c1b99690ec94cf54d0f5c573097f31cac6
Minh To <minhto@Minhs-MacBook-Pro.local>
2019-09-06 15:32:28 -0400

    reversed changes from last commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello jhhjhh!");
+    res.send("Hello world!");
 });
 
 // setup http server to listen on HTTP_PORT
